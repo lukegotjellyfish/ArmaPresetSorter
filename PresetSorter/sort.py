bf2faa4d02a5ba50b6e0a1817362c24df7551941
Neko <lukegotjellyfish@gmail.com>
2019-04-04 19:26:16 +0100

    Update sort.py

--- PresetSorter/sort.py
+++ PresetSorter/sort.py
@@ -1,41 +1,40 @@
-class stripper:
-    def strip_header():
-        start = 1449 #end of header
-        end = -149 #start of footer
-        with open("Arma.html", "r", encoding='utf-8') as modfile:
-            contents = modfile.readlines()
-            header = ''.join(contents)[:start] + "\n"
-            footer = ''.join(contents)[end:]
+def strip_header():
+    start = 1449 #end of header
+    end = -149 #start of footer
+    with open("Arma.html", "r", encoding='utf-8') as modfile:
+        contents = modfile.readlines()
+        header = ''.join(contents)[:start] + "\n"
+        footer = ''.join(contents)[end:]
 
 
-            #Get lines past the header
-            i = 1
-            mods = []
-            for line in contents:
-                if i > 83:
-                    mods.append(line)
-                else:
-                    i += 1
-                    continue
+        #Get lines past the header
+        i = 1
+        mods = []
+        for line in contents:
+            if i > 83:
+                mods.append(line)
+            else:
                 i += 1
-                
-            #Get the lines with the <tr> for each mod (exactly the same upto the mod name)
-            mod_array = []
-            mod_lines = ""
-            i = 1
-            local = 0
-            for item in mods:
-                mod_lines += item
-                if item == "        </tr>\n":
-                    mod_array.append(mod_lines)
-                    mod_lines = ""
-                i += 1
-            #Sort the array by character value
-            mod_array_new = sorted(mod_array) #sorted mods "alphabetically"
-            sorted_mods = header + ''.join(mod_array_new) + footer
-            return sorted_mods
+                continue
+            i += 1
+
+        #Get the lines with the <tr> for each mod (exactly the same upto the mod name)
+        mod_array = []
+        mod_lines = ""
+        i = 1
+        local = 0
+        for item in mods:
+            mod_lines += item
+            if item == "        </tr>\n":
+                mod_array.append(mod_lines)
+                mod_lines = ""
+            i += 1
+        #Sort the array by character value
+        mod_array_new = sorted(mod_array) #sorted mods "alphabetically"
+        sorted_mods = header + ''.join(mod_array_new) + footer
+        return sorted_mods
 
 with open("Sorted_Preset.html", "w", encoding='utf-8') as mods_writefile:
-    mods_writefile.write(str(stripper.strip_header()))
+    mods_writefile.write(strip_header())
     print("dun")
     #Write the sorted array into a file that works as a preset
