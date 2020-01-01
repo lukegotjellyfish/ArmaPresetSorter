af506c01f5b9e71ea8f092698ebdadea5e4d24c6
lukegotjellyfish <lukegotjellyfish@gmail.com>
2020-01-01 12:02:58 +0000

    Char -> Line based
    dynamic for preset name/desc

--- PresetSorter/sort.py
+++ PresetSorter/sort.py
@@ -1,16 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import os
+import tkinter as tk
+from tkinter import filedialog
+from time import sleep
+
+
 def strip_header():
-    start = 1447 #end of header
-    end = -149 #start of footer
-    with open("Arma.html", "r", encoding='utf-8') as modfile:
+    start = 88 #end of header
+    end   = -10 #start of footer
+
+    root = tk.Tk()
+    root.withdraw()
+    file_path = filedialog.askopenfilename(initialdir=os.getcwd(), title="Select Preset to Inherit", filetypes=[("HTML Preset", "*.html")])
+    with open(file_path, "r", encoding='utf-8') as modfile:
         contents = modfile.readlines()
-        header = ''.join(contents)[:start] + "\n"
-        footer = ''.join(contents)[end:]
+        header = contents[:start]
+        footer = contents[end:]
+        #print(header)
+        #print(footer)
 
         #Get lines past the header
         i = 1
         mods = []
         for line in contents:
-            if i > 83:
+            if i > start:
                 mods.append(line)
             else:
                 i += 1
@@ -20,21 +34,21 @@ def strip_header():
         #Get the lines with the <tr> for each mod (exactly the same upto the mod name)
         mod_array = []
         mod_lines = ""
-        i = 1
-        local = 0
         for item in mods:
-            mod_lines += item
-            if item == "        </tr>\n":
-                mod_array.append(mod_lines)
-                mod_lines = ""
-            i += 1
+            mod_lines += item  #Add mod section contents to string
+            if item == "        </tr>\n":  #If the end of the mod section is reached
+                mod_array.append(mod_lines)  #Append mod table body to array of mods for sorting
+                mod_lines = ""  #Clear mod section from string at end of table section
         #Sort the array by character value
         mod_array_new = sorted(mod_array) #sorted mods "alphabetically"
-        sorted_mods = header + ''.join(mod_array_new) + footer
+        sorted_mods = ''.join(header) + ''.join(mod_array_new) + ''.join(footer)
         return sorted_mods
 
 
 #Write the sorted array into a file that works as a preset
-with open("Sorted_Preset.html", "w", encoding='utf-8') as mods_writefile:
+file_path = input("Name of new Preset:\n> ")
+with open("./Output/" + file_path + ".html", "w", encoding='utf-8') as mods_writefile:
     mods_writefile.write(strip_header())
-x = input("Done")
+
+print("Done")
+sleep(2)
